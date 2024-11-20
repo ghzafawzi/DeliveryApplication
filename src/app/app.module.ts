15754520409094458a0554bb71c08641968ddd9e
IdrisBouar <102695491+IdrisBouar@users.noreply.github.com>
2024-11-20 16:38:16 +0100

    SignUp & Login Functions !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
+  providers: [{ 
+    provide: RouteReuseStrategy,
+    useClass: IonicRouteStrategy
+  }],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import { AlertController } from '@ionic/angular';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
- 
+  email: string = '';
+  password: string = '';
+  private firestore = getFirestore(initializeApp(environment.firebase));
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  async login() {
+    const userRef = collection(this.firestore, 'utilisateur');
+    const q = query(userRef, where('email', '==', this.email), where('password', '==', this.password));
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      this.showAlert('Login Failed', 'Invalid email or password');
+    } else {
+      this.showAlert('Login Successful', 'Welcome back!');
+    }
   }
 
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
